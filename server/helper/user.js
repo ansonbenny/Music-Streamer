6053d44b71afb362f5c1f5f9de537b88bf7d4153
anson <ansonbenny16@gmail.com>
2023-05-10 22:13:07 +0530

    login forgot server

--- server/helper/user.js
+++ server/helper/user.js
@@ -63,6 +63,58 @@ export default {
       }
     });
   },
+  register_complete: (id, secret) => {
+    return new Promise(async (resolve, reject) => {
+      let response;
+      try {
+        await db
+          .collection(collections.USER)
+          .createIndex({ email: 1 }, { unique: true });
+
+        let already = await db.collection(collections.USER).findOne({
+          _id: new ObjectId(id),
+        });
+
+        if (!already) {
+          let temp = await db.collection(collections.TEMP).findOne({
+            _id: new ObjectId(id),
+            secret,
+          });
+
+          if (temp) {
+            delete temp.secret;
+            delete temp.expireAt;
+
+            temp.email = temp.email.replace("_register", "");
+
+            response = await db.collection(collections.USER).insertOne(temp);
+          } else {
+            reject({ status: 422, message: "Wrong Verification Details" });
+          }
+        } else {
+          reject({ status: 422, message: "Already Registered" });
+        }
+      } catch (err) {
+        if (err?.code === 11000) {
+          reject({ status: 422, message: "Already Registered" });
+        } else {
+          reject(err);
+        }
+      } finally {
+        if (response) {
+          await db
+            .collection(collections.TEMP)
+            .deleteOne({
+              _id: new ObjectId(id),
+            })
+            .catch((err) => {
+              console.log("Temp Delete Error : ", err);
+            });
+          resolve(response);
+        }
+      }
+    });
+  },
   login_manual: (email, password) => {
     return new Promise(async (resolve, reject) => {
       try {
@@ -85,6 +137,126 @@ export default {
         }
       } catch (err) {
         reject(err);
+      }
+    });
+  },
+  get_user: (userId) => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let user = await db.collection(collections.USER).findOne({
+          _id: new ObjectId(userId),
+        });
+
+        if (user) {
+          delete user?.password;
+          resolve(user);
+        } else {
+          reject({ status: 404 });
+        }
+      } catch (err) {
+        reject(err);
+      }
+    });
+  },
+  forgot_request: (details) => {
+    return new Promise(async (resolve, reject) => {
+      let response;
+
+      try {
+        await db
+          .collection(collections.TEMP)
+          .createIndex({ email: 1 }, { unique: true });
+        await db
+          .collection(collections.TEMP)
+          .createIndex({ expireAt: 1 }, { expireAfterSeconds: 3600 });
+
+        let check = await db.collection(collections.USER).findOne({
+          email: details.email.replace("_forgot", ""),
+        });
+
+        if (check) {
+          details.password = await bcrypt.hash(details.password, 10);
+
+          response = await db.collection(collections.TEMP).insertOne({
+            ...details,
+            _id: check._id,
+            expireAt: new Date(),
+          });
+        } else {
+          reject({ status: 405, message: "User Not Found" });
+        }
+      } catch (err) {
+        if (err?.code === 11000) {
+          response = await db
+            .collection(collections.TEMP)
+            .findOneAndUpdate(
+              {
+                email: details.email,
+              },
+              {
+                $set: {
+                  ...details,
+                  expireAt: new Date(),
+                },
+              }
+            )
+            .catch((err_2) => {
+              console.log(err_2);
+              reject(err_2);
+            });
+        } else {
+          console.log(err);
+          reject(err);
+        }
+      } finally {
+        if (response) {
+          if (response?.insertedId) {
+            resolve({ _id: response.insertedId.toString() });
+          } else if (response?.value?._id) {
+            resolve({ _id: response.value._id.toString() });
+          }
+        }
+      }
+    });
+  },
+  forgot_complete: (id, secret) => {
+    return new Promise(async (resolve, reject) => {
+      let response;
+      try {
+        let temp = await db.collection(collections.TEMP).findOne({
+          _id: new ObjectId(id),
+          secret,
+        });
+
+        if (temp) {
+          response = await db.collection(collections.USER).updateOne(
+            {
+              _id: new ObjectId(id),
+            },
+            {
+              $set: {
+                password: temp.password,
+              },
+            }
+          );
+        } else {
+          reject({ status: 422, message: "Wrong Verification Details" });
+        }
+      } catch (err) {
+        reject(err);
+      } finally {
+        if (response) {
+          await db
+            .collection(collections.TEMP)
+            .deleteOne({
+              _id: new ObjectId(id),
+            })
+            .catch((err) => {
+              console.log("Temp Delete Error : ", err);
+            });
+
+          resolve(response);
+        }
       }
     });
   },
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,7 +5,37 @@ import jwt from "jsonwebtoken";
 
 let router = Router();
 
-router.post("/register", async (req, res) => {
+const CheckLogged = (req, res, next) => {
+  const { token = null } = req.cookies;
+  jwt.verify(token, process.env.JWT_SECRET, async (err, decode) => {
+    if (decode?._id?.length === 24) {
+      try {
+        let userData = await user.get_user(decode?._id);
+
+        if (userData) {
+          res.status(208).json({
+            status: 208,
+            message: "Already Logged",
+            data: userData,
+          });
+        }
+      } catch (err) {
+        console.log(err);
+        res.clearCookie("token");
+        next();
+      }
+    } else if (err) {
+      console.log(`Error : ${err?.name}`);
+      res.clearCookie("token");
+      next();
+    } else {
+      res.clearCookie("token");
+      next();
+    }
+  });
+};
+
+router.post("/register", CheckLogged, async (req, res) => {
   let { name, email, password, rePassword, google } = req.body;
   if (google) {
   } else {
@@ -72,11 +102,138 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.get("/login", async (req, res) => {
-  const { email, password, google } = req.query;
+router.post("/register-complete", CheckLogged, async (req, res) => {
+  const { id, secret } = req.body;
+
+  if (id?.length === 24 && secret) {
+    let response;
+    try {
+      response = await user.register_complete(id, secret);
+    } catch (err) {
+      if (err?.status) {
+        res.status(err.status).json(err);
+      } else {
+        res.status(500).json({
+          status: 500,
+          message: err,
+        });
+      }
+    } finally {
+      if (response) {
+        res.status(200).json({
+          status: 200,
+          message: "Register Completed",
+        });
+      }
+    }
+  } else {
+    res.status(422).json({
+      status: 422,
+      message: "Wrong Verification Details",
+    });
+  }
+});
+
+router.post("/forgot", CheckLogged, async (req, res) => {
+  let { email, password, rePassword } = req.body;
+  if (email) {
+    if (password?.length >= 8 && password === rePassword) {
+      email = email.toLowerCase();
+      let secret = Math.random().toString(16);
+
+      let response;
+
+      try {
+        response = await user.forgot_request({
+          email: `${email}_forgot`,
+          password,
+          secret,
+        });
+      } catch (err) {
+        if (err?.status) {
+          res.status(err.status).json(err);
+        } else {
+          res.status(500).json({
+            status: 500,
+            message: err,
+          });
+        }
+      } finally {
+        if (response?._id) {
+          sendMail(
+            {
+              to: email,
+              subject: `Musicon Password Forgot Verification`,
+              html: `You can complete password forgot by clicking below link  <a href="${process.env.SITE_URL}/forgot/pending/${response._id}/${secret}" >Click</a>`,
+            },
+            (err, done) => {
+              if (err) {
+                console.log(err);
+              } else {
+                console.log(`Email sent: ${done.response}`);
+              }
+            }
+          );
+
+          res.status(200).json({
+            status: 200,
+            message: "Forgot Request Sented",
+          });
+        }
+      }
+    } else {
+      res.status(422).json({
+        status: 422,
+        message:
+          "Password length must contain 8 and password and Re Enter password must same",
+      });
+    }
+  } else {
+    res.status(422).json({
+      status: 422,
+      message: "Enter email",
+    });
+  }
+});
+
+router.put("/forgot-complete", CheckLogged, async (req, res) => {
+  const { id, secret } = req.body;
+  if (id?.length === 24 && secret) {
+    let response;
+    try {
+      response = await user.forgot_complete(id, secret);
+    } catch (err) {
+      if (err?.status) {
+        res.status(err.status).json(err);
+      } else {
+        res.status(500).json({
+          status: 500,
+          message: err,
+        });
+      }
+    } finally {
+      if (response) {
+        res.status(200).json({
+          status: 200,
+          message: "Success",
+        });
+      }
+    }
+  } else {
+    res.status(422).json({
+      status: 422,
+      message: "Wrong Verification Details",
+    });
+  }
+});
+
+router.get("/login", CheckLogged, async (req, res) => {
+  let { email, password, google } = req.query;
   if (google) {
   } else {
     if (email && password?.length >= 8) {
+      email = email.toLowerCase();
+
       let response;
 
       try {
@@ -108,6 +265,7 @@ router.get("/login", async (req, res) => {
             .json({
               status: 200,
               message: "Success",
+              data: response,
             });
         }
       }
@@ -118,6 +276,13 @@ router.get("/login", async (req, res) => {
       });
     }
   }
+});
+
+router.get("/logout", (req, res) => {
+  res.status(200).clearCookie("token").json({
+    status: 200,
+    message: "LogOut",
+  });
 });
 
 export default router;
